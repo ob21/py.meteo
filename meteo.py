ca3cf5013c63e9efeea41e0529871a7da1208767
OPOB7414 <olivier.briand@orange.com>
2017-06-01 10:18:44 +0200

    manage a list of cities to get meteo

--- meteo.py
+++ meteo.py
@@ -5,48 +5,56 @@ session = requests.Session()
 session.trust_env = False
 
 api_key = "56be7192b69f7116538006ce67cd4e3c"
-city = "Ruoms"
-base_url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&lang=fr&units=metric"
+base_url = "http://api.openweathermap.org/data/2.5/weather?q="
+params_base = "&lang=fr&units=metric"
 
-# build the url
-url = base_url + "&APPID=" + api_key
+cities = ["Ruoms", "Rennes", "Royan"]
 
-# make the http request to openweathermap
-print("request " + url)
-proxy = {"http": "http://p-goodway.rd.francetelecom.fr:3128"}
-r = session.get(url, proxies=proxy)
-# r = session.get(url)
-print("response = " + r.text)
+def getMeteoFor(c):
+    # build the url
+    url = base_url + c + params_base + "&APPID=" + api_key
 
-# parse the json and get the data
-data = json.loads(r.text);
-print()
-city = data['name']
-main = data['weather'][0]['main']
-description = data['weather'][0]['description']
-temp = data['main']['temp']
-pressure = data['main']['pressure']
-temp_min = data['main']['temp_min']
-temp_max = data['main']['temp_max']
-humidity = data['main']['humidity']
-wind_speed = data['wind']['speed'];
+    # make the http request to openweathermap
+    print("request " + url)
+    proxy = {"http": "http://p-goodway.rd.francetelecom.fr:3128"}
+    r = session.get(url, proxies=proxy)
+    # r = session.get(url)
+    print("response = " + r.text)
 
-print("Météo de " + city + " : ");
-print(main);
-print(description);
-print("temp = " + str(temp));
-print("pressure = " + str(pressure));
-print("temp_min = " + str(temp_min));
-print("temp_max = " + str(temp_max));
-print("humidity = " + str(humidity));
-print("wind_speed = " + str(wind_speed));
+    # parse the json and get the data
+    data = json.loads(r.text);
+    print()
+    city = data['name']
+    main = data['weather'][0]['main']
+    description = data['weather'][0]['description']
+    temp = data['main']['temp']
+    pressure = data['main']['pressure']
+    temp_min = data['main']['temp_min']
+    temp_max = data['main']['temp_max']
+    humidity = data['main']['humidity']
+    wind_speed = data['wind']['speed'];
 
-# create a entry on the obriand bdd
-print()
-data = {'city': city, 'main': main, 'description': description, 'temp': temp, 'pressure': pressure, 'humidity': humidity, 'temp_min': temp_min, 'temp_max': temp_max, 'wind_speed': wind_speed}
-dataUrl = "http://obriand.fr/meteo/add.php"
-print("request post data = " + dataUrl + " with " + str(data))
-r = session.post(dataUrl, data=data, proxies=proxy)
-# r = session.post(dataUrl, data=data)
-print("response data = " + r.text)
+    print("Météo de " + c + " : ");
+    print(main);
+    print(description);
+    print("temp = " + str(temp));
+    print("pressure = " + str(pressure));
+    print("temp_min = " + str(temp_min));
+    print("temp_max = " + str(temp_max));
+    print("humidity = " + str(humidity));
+    print("wind_speed = " + str(wind_speed));
+
+    # create a entry on the obriand bdd
+    print()
+    data = {'city': city, 'main': main, 'description': description, 'temp': temp, 'pressure': pressure, 'humidity': humidity, 'temp_min': temp_min, 'temp_max': temp_max, 'wind_speed': wind_speed}
+    dataUrl = "http://obriand.fr/meteo/add.php"
+    print("request post data = " + dataUrl + " with " + str(data))
+    r = session.post(dataUrl, data=data, proxies=proxy)
+    # r = session.post(dataUrl, data=data)
+    print("response data = " + r.text)
+    print()
+
+for c in cities:
+    getMeteoFor(c)
+
 
